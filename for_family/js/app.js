eee444544192c87170ed19f50a38bd4afb54d0db
az8321550 <az8321550@gmail.com>
2016-05-17 17:31:05 +0800

    Immigration

---
+++ for_family/js/app.js
@@ -0,0 +1,78 @@
+
+var flag1=false;
+
+var mySwiper= new Swiper('.swiper-container',{
+  resistance:"100%",
+  initialSlide:3,
+  onTouchStart:function(swiper){
+    if(swiper.activeIndex==2){
+      if(!flag1){
+        swiper.lockSwipes()
+        $(".bg3-active").addClass("active");
+        flag1=true;
+        setTimeout(function(){
+          mySwiper.unlockSwipes()
+        },200)
+      }
+    }
+
+    return false;
+  },
+
+});
+mySwiper.lockSwipes()
+
+var loading=$(".loading");
+var i=0;
+var time=setInterval(function(){
+  i++;
+  loading.text(i+"%")
+  if(i>=100){
+    clearInterval(time)
+    mySwiper.unlockSwipes()
+    mySwiper.slidePrev()
+  }
+},70)
+
+
+//确认上传
+$(".btn-up").click(function () {
+  if($('#form').find("input").val()){
+
+    $(".up-con").eq(1).addClass("active").siblings().removeClass("active");
+
+
+    // 上传代码
+    /*jQuery('#form').ajaxSubmit({
+      url: '/file',
+      success:function(result) {
+        alert(result)
+      }
+    });*/
+
+  }else{
+    //alert("请先上传图片");
+    $(".up-con").eq(1).addClass("active").siblings().removeClass("active");
+  }
+
+})
+
+$(".share").click(function(){
+  $(".bg-share").addClass("active");
+})
+
+
+//上传图片
+$(".btn-up-con").click(function(){
+  $('#form').find("input").click()
+})
+
+$(".btn-coupon").click(function(){
+  $(".coupon").addClass("active");
+})
+
+$(".coupon-close").click(function(){
+  $(".coupon").removeClass("active");
+})
+
+
