d15c08fa4e84d8277af35ab0323c6177adb2e662
az8321550 <az8321550@gmail.com>
2016-05-17 21:50:15 +0800

    Update

--- woniu/js/app.js
+++ woniu/js/app.js
@@ -1,7 +1,15 @@
 var mySwiper= new Swiper('.swiper-container',{
   resistance:"100%",
   direction:"vertical",
-  initialSlide:0
+  initialSlide:2,
+  onTransitionEnd:function(swiper){
+    if(swiper.activeIndex==1){
+      $(".logo").addClass("active")
+    }
+    if(swiper.activeIndex==2){
+      $(".logo2").addClass("active")
+    }
+  }
 });
 
 $(".page1--center").addClass("active")
