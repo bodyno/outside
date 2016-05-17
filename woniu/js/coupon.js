7bfb627340d73046bc39eeacead12ac53c435982
az8321550 <az8321550@gmail.com>
2016-05-17 21:52:40 +0800

    Update

--- woniu/js/coupon.js
+++
@@ -1,8 +0,0 @@
-$(".coupon-rule-btn2").click(function(){
-  $(".coupon-bg").addClass("active").siblings().removeClass("active")
-})
-
-
-$(".coupon-item").click(function(){
-  $(".coupon-rule-con").addClass("active").siblings().removeClass("active")
-})
